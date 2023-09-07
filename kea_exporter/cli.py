d04276b32d139b356eafcacfca77471d2b70ffbf
Jakob Mellberg <jakob.mellberg@oru.se>
2023-09-07 14:31:27 +0200

    Default to socket mode

--- kea_exporter/cli.py
+++ kea_exporter/cli.py
@@ -6,11 +6,11 @@ from prometheus_client import start_http_server
 from . import __PROJECT__, __VERSION__
 
 @click.command()
-@click.argument("mode", envvar='MODE', type=click.Choice(['socket', 'http'], case_sensitive=True), required=True)
+@click.option('-m', '--mode', envvar='MODE', default='socket', type=click.Choice(['socket', 'http'], case_sensitive=True))
 @click.option('-a', '--address', envvar='ADDRESS', default='0.0.0.0', help='Specify the address to bind against.')
 @click.option('-p', '--port', envvar='PORT', type=int, default=9547, help='Specify the port on which to listen.')
 @click.option('-i', '--interval', envvar='INTERVAL', type=int, default=7.5, help='Specify the metrics update interval in seconds.')
-@click.option('-t', '--target', envvar='TARGET', type=str, default=7.5, help='Target address and port of Kea server, e.g. http://kea.example.com:8080.')
+@click.option('-t', '--target', envvar='TARGET', type=str, help='Target address and port of Kea server, e.g. http://kea.example.com:8080.')
 @click.argument('sockets', envvar='SOCKETS', nargs=-1, required=False)
 @click.version_option(prog_name=__PROJECT__, version=__VERSION__)
 def cli(mode, port, address, interval, **kwargs):
